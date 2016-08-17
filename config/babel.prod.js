621f8d8a3bc91c3c49b406afa92e2f2f83c7109d
Vinicius Dallacqua <vinicius.dallacqua@bonniergaming.com>
2016-08-17 11:56:44 +0200

    Add NPM and Webpack to the build system
    Now this is all run by NPM and Webpack. A proper build system in place
    and managed dependencies to avoid `vendor` bloat on commit history. Also
    this allows us to use ES2015/2016/Next and cleanup a bit more our code.

---
+++ config/babel.prod.js
@@ -0,0 +1,18 @@
+module.exports = {
+  babelrc: false,
+  presets: [
+    'babel-preset-es2015',
+    'babel-preset-es2016',
+  ].map(require.resolve),
+  plugins: [
+    'babel-plugin-syntax-trailing-function-commas',
+    'babel-plugin-transform-class-properties',
+    'babel-plugin-transform-object-rest-spread',
+  ].map(require.resolve).concat([
+    [require.resolve('babel-plugin-transform-runtime'), {
+      helpers: false,
+      polyfill: false,
+      regenerator: true
+    }]
+  ])
+};
